5891d9a4e074f72553187a0608ace56dfb1e0659
MahadiMunna <mahadimunna.official@gmail.com>
2025-06-30 16:47:59 +0600

    Lambda from java brains

---
+++ Lambda/src/TypeInterface.java
@@ -0,0 +1,13 @@
+public class TypeInterface {
+    public static void main(String[] args) {
+        StringLengthLambda myLambda = s -> s.length();
+        System.out.println(myLambda.getLength("Hello World"));
+        printLambda(s -> s.length());
+    }
+    public static void printLambda(StringLengthLambda l){
+        System.out.println(l.getLength("Hello World"));
+    }
+    interface StringLengthLambda{
+        int  getLength(String s);
+    }
+}
